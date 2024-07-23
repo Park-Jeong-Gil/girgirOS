e6efad7d1e69650944fdb26a59c7107cd5e27256
girgir <wjdrlf5986@naver.com>
2024-07-23 13:26:24 +0900

    background update

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -10,25 +10,31 @@ function Background() {
   return (
     <div className="Background">
       <button className="desktopIcon myPc">
-        <span>내 컴퓨터</span>
+        <span className="iconImage"></span>
+        <span className="iconName">내 컴퓨터</span>
       </button>
       <button className="desktopIcon myDoc">
-        <span>내 문서</span>
+        <span className="iconImage"></span>
+        <span className="iconName">내 문서</span>
       </button>
       <button className="desktopIcon trashCan">
-        <span>휴지통</span>
+        <span className="iconImage"></span>
+        <span className="iconName">휴지통</span>
       </button>
       <button className="desktopIcon ie">
-        <span>Internet<br/> Explorer</span>
+        <span className="iconImage"></span>
+        <span className="iconName">Internet<br/> Explorer</span>
       </button>
       <button className="desktopIcon outlook">
-        <span>outlook<br/> Express</span>
+        <span className="iconImage"></span>
+        <span className="iconName">Outlook<br/> Express</span>
       </button>
       <button className="desktopIcon help">
-        <span>도움말</span>
+        <span className="iconImage"></span>
+        <span className="iconName">도움말</span>
       </button>
+
     </div>
-    
   )
 }
 
