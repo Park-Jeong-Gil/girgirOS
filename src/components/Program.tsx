a77c15809d0d7c378072229e0908e304edd305c6
girgir <wjdrlf5986@naver.com>
2024-07-29 18:53:36 +0900

    program update

--- src/components/Program.tsx
+++ src/components/Program.tsx
@@ -13,10 +13,15 @@ function Program({ name, programId }: ProgramProps) {
   const [programArr, setProgramArr] = useRecoilState(programStatus);
   const [activeProgram, setActiveProgram] = useRecoilState(currentProgram);
 
-  const [size, setSize] = useState({ width: 400, height: 300 });
+  const winW = window.innerWidth
+  const winH = window.innerHeight
+
+  const [size, setSize] = useState({ width: winW/2, height: winH/2 });
   const [isResizing, setIsResizing] = useState(false);
   const [resizeDirection, setResizeDirection] = useState<null | string>(null);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isMinimized, setIsMinimized] = useState(false);
+  const [isMaximized, setIsMaximized] = useState(false);
+  const [position, setPosition] = useState({ x: ((winW/2) - (size.width/2)), y: ((winH/2) - (size.height/2))});
   const windowRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -92,68 +97,88 @@ function Program({ name, programId }: ProgramProps) {
     };
   }, [isResizing]);
 
+  useEffect(() => {
+    console.log("isMinimized : ", isMinimized, "isMaximized : ", isMaximized);
+  }, [isMinimized, isMaximized]);
+
+  const handleMinimize = () => {
+    setIsMinimized(!isMinimized);
+  };
+
+  const handleMaximize = () => {
+    setIsMaximized(!isMaximized);
+  };
+
+  const handleClose = () => {
+    setProgramArr((prev) => prev.filter((prog) => prog.program !== programId));
+  };
+
   return (
     <Draggable
       handle=".title-bar"
-      bounds="parent"
-      position={position}
+      defaultPosition={position}
       onDrag={(e, data) => setPosition({ x: data.x, y: data.y })}
     >
       <div
-        className="ProgramWindow window"
+        className={`ProgramWindow window${isMinimized ? ' minimized' : ''}${isMaximized ? ' maximized' : ''}`}
         id={programId || ""}
         ref={windowRef}
         style={{
-          width: size.width,
-          height: size.height,
-          transform: `translate(${position.x}px, ${position.y}px)`,
+          width: isMaximized ? "100%" : size.width,
+          height: isMaximized ? "100%" : size.height,
+          top: isMaximized ? 0 : position.y,
+          left: isMaximized ? 0 : position.x,
         }}
       >
         <div className="container">
           <div className="title-bar">
             <h3 className="title-bar-text">{name}</h3>
             <div className="title-bar-controls">
-              <button aria-label="Minimize"></button>
-              <button aria-label="Maximize"></button>
-              <button aria-label="Close"></button>
+              <button aria-label="Minimize" onClick={handleMinimize}></button>
+              <button aria-label={`${isMaximized ? 'Restore' : 'Maximize'}`} onClick={handleMaximize}></button>
+              <button aria-label="Close" onClick={handleClose}></button>
             </div>
           </div>
           <div className="window-body">
             <p>There's so much room for activities!</p>
           </div>
         </div>
-        <span
-          className="resize-handle right"
-          onMouseDown={() => handleMouseDown("right")}
-        />
-        <span
-          className="resize-handle bottom"
-          onMouseDown={() => handleMouseDown("bottom")}
-        />
-        <span
-          className="resize-handle bottom-right"
-          onMouseDown={() => handleMouseDown("bottom-right")}
-        />
-        <span
-          className="resize-handle left"
-          onMouseDown={() => handleMouseDown("left")}
-        />
-        <span
-          className="resize-handle top"
-          onMouseDown={() => handleMouseDown("top")}
-        />
-        <span
-          className="resize-handle top-left"
-          onMouseDown={() => handleMouseDown("top-left")}
-        />
-        <span
-          className="resize-handle top-right"
-          onMouseDown={() => handleMouseDown("top-right")}
-        />
-        <span
-          className="resize-handle bottom-left"
-          onMouseDown={() => handleMouseDown("bottom-left")}
-        />
+        {isMaximized === false && (
+          <div className="handleWrap">
+            <span
+              className="resize-handle right"
+              onMouseDown={() => handleMouseDown("right")}
+            />
+            <span
+              className="resize-handle bottom"
+              onMouseDown={() => handleMouseDown("bottom")}
+            />
+            <span
+              className="resize-handle bottom-right"
+              onMouseDown={() => handleMouseDown("bottom-right")}
+            />
+            <span
+              className="resize-handle left"
+              onMouseDown={() => handleMouseDown("left")}
+            />
+            <span
+              className="resize-handle top"
+              onMouseDown={() => handleMouseDown("top")}
+            />
+            <span
+              className="resize-handle top-left"
+              onMouseDown={() => handleMouseDown("top-left")}
+            />
+            <span
+              className="resize-handle top-right"
+              onMouseDown={() => handleMouseDown("top-right")}
+            />
+            <span
+              className="resize-handle bottom-left"
+              onMouseDown={() => handleMouseDown("bottom-left")}
+            />
+          </div>
+        )}
       </div>
     </Draggable>
   );
