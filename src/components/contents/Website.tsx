3d5308286ce7351c15b6378fc52a430b3db0cb07
girgir <wjdrlf5986@naver.com>
2024-08-19 21:46:55 +0900

    web site update

--- src/components/contents/Website.tsx
+++ src/components/contents/Website.tsx
@@ -104,7 +104,25 @@ function Website({}: WebsiteProps) {
             </ul>
           </div>
           <p className="descTit">사이트 설명 :</p>
-          <article className="websiteDesc pageDesc" ref={descriptionRef} dangerouslySetInnerHTML={{ __html: activeDescription }}></article>
+          {activeSrc ? 
+            <article className="websiteDesc pageDesc" ref={descriptionRef} dangerouslySetInnerHTML={{ __html: activeDescription }}></article> :
+            <article className="websiteDesc pageDesc">
+              <header className="siteHeader">
+                <h3>프로젝트 : <strong>Girgir OS</strong></h3>
+                <p>
+                  작업 기간 : 
+                  <time dateTime="2024-03">2024년 7월</time> ~ 
+                  <time dateTime="2024-04">2024년 8월</time>
+                </p>
+                <p className="skill">사용기술 : Vite, React, Typescript</p>
+              </header>
+              <div className="siteDesc">
+                <p>현재 포트폴리오 사이트는 <strong>90년대 Windows</strong>를 컨셉으로 작업 했습니다.</p>
+                <p>제가 레트로한 감성을 좋아하기도 하고, 호불호가 갈릴 수도 있지만 조금 더 기억에 남을 수 있게 재미있는 포트폴리오 사이트를 만들고 싶었습니다. </p>
+                <p>그동안 재직 중 제 관심 분야였던 스크롤 인터렉션을 만드는 작업과 퍼포먼스를 최적화 하는 경험은 많이 있었지만, 아쉽게도 프론트 엔드 기술을 활용 할 수 있는 프로젝트를 접할 기회가 없었습니다. 때문에 혼자서 React와 Typescript를 틈틈히 공부했고 이번 포트폴리오에서 Vite, React, Typescript를 사용하여 작업 할 수 있었습니다.</p>
+              </div>
+            </article>
+          }
         </div>
       </div>
       <div className="closeBtnWrap">
